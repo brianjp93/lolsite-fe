f75e615794d0137f91718be5ebab935ef4b9b716
Brian Perrett <bperrett@fusionbox.com>
2023-10-19 15:24:47 -0700

    Add ga4 tracking

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -17,6 +17,7 @@ export const serverSchema = z.object({
 export const clientSchema = z.object({
   // NEXT_PUBLIC_CLIENTVAR: z.string(),
   NEXT_PUBLIC_BACKEND_URL: z.string(),
+  NEXT_PUBLIC_MEASUREMENT_ID: z.string(),
 });
 
 /**
@@ -28,4 +29,5 @@ export const clientSchema = z.object({
 export const clientEnv = {
   // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
+  NEXT_PUBLIC_MEASUREMENT_ID: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 };
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,15 +1,29 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { env } from "@/env/client.mjs";
+import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html>
       <Head>
-        <link rel="shortcut icon" type="image/png" href="/gen/logo-clean.png"/>
+        <link rel="shortcut icon" type="image/png" href="/gen/logo-clean.png" />
+        <Script
+          id="ga4-script"
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=${env.NEXT_PUBLIC_MEASUREMENT_ID}`}
+        >
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${env.NEXT_PUBLIC_MEASUREMENT_ID}');
+        `}
+        </Script>
       </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
