48ec93493f5ef9ef06a211cc683d693a5349a8cc
Brian Perrett <perrettbrian@gmail.com>
2026-08-15 19:30:55 -0700

    don't need the useeffect

--- src/pages/puuid/[puuid]/index.tsx
+++ src/pages/puuid/[puuid]/index.tsx
@@ -1,7 +1,6 @@
 import Skeleton from "@/components/general/skeleton";
 import { useRouter } from "next/router";
 import { useSummonerByPuuid } from "@/hooks";
-import { useEffect } from "react";
 import { profileRoute } from "@/routes";
 
 // used as a redirect page so we can link directly to a puuid
@@ -11,16 +10,14 @@ export default function PuuidPage() {
   const summonerQuery = useSummonerByPuuid({ puuid });
   const summoner = summonerQuery.data;
 
-  useEffect(() => {
-    if (summoner) {
-      const url = profileRoute({
-        region: summoner.region,
-        riotIdName: summoner.riot_id_name,
-        riotIdTagline: summoner.riot_id_tagline,
-      });
-      router.replace(url);
-    }
-  }, [summoner, router]);
+  if (summoner) {
+    const url = profileRoute({
+      region: summoner.region,
+      riotIdName: summoner.riot_id_name,
+      riotIdTagline: summoner.riot_id_tagline,
+    });
+    router.replace(url);
+  }
   return (
     <Skeleton>
       {summonerQuery.isLoading && <div>Loading...</div>}
